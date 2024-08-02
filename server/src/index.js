5ec160c3b838c990e7dbb1b9d4ad87a28c36f43f
dhruvtiwari6 <dhurvtiwari6@gmail.com>
2024-08-02 12:42:56 +0530

    first commit

--- server/src/index.js
+++ server/src/index.js
@@ -3,10 +3,10 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 
-const { connectDB } = require('./src/db/connection.js');
-const { router } = require('./src/route/user.route.js');
-const chatRouter = require('./src/route/chatRoute.js');
-const messageRouter = require('./src/route/messageRoute.js');
+const { connectDB } = require('./db/connection.js');
+const { router } = require('./route/user.route.js');
+const chatRouter = require('./route/chatRoute.js');
+const messageRouter = require('./route/messageRoute.js');
 
 const app = express();
 
@@ -27,7 +27,7 @@ const http = require('http');
 const server = http.createServer(app);
 
 const { Server } = require("socket.io");
-const { chat } = require('./src/model/chat.model.js');
+const { chat } = require('./model/chat.model.js');
 const io = new Server(server, {
     pingTimeout: 60000, 
     cors: {
